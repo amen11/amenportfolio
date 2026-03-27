a1551d43f7c6d0ff31b6ecf674d5339e38de8e32
Amen Korchane <41312751+amen11@users.noreply.github.com>
2026-03-27 23:52:14 +0100

    build

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-
+import { Analytics } from '@vercel/analytics/react';
 export default function App() {
   return (
     <>
+    <Analytics />
       <Nav />
       <Hero />
       <div className="divider" />
@@ -21,6 +22,7 @@ export default function App() {
       <Projects />
       <div className="divider" />
       <Contact />
+
     </>
   );
 }
